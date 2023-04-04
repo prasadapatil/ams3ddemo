0fc8fd8066aadb831d78addf0a38b4039e448feb
prasadpatil <prasad.a.patil@outlook.com>
2023-04-05 01:27:08 +0530

    added final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+import Login from "./Login";
+
+export default function App() {
+  const [isLogin, setIsLogin] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {!isLogin ? <Login isLogin={isLogin} setIsLogin={setIsLogin}/> : <div class="sketchfab-embed-wrapper"> <iframe title="SHIVAJI MAHARAJ_(13-06-2022)_thin" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share width="640" height="480" src="https://sketchfab.com/models/39c8e01cc4fc47d0a4c3889c8271ca78/embed?annotations_visible=1&annotation_cycle=5"> 
+      </iframe> 
+      <button onClick={()=>setIsLogin(false)} >Logout</button>
+      </div>}
     </div>
   );
 }
-
-export default App;
---
+++ src/Login.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import './login.css'
+const Login = ({isLogin, setIsLogin}) => {
+    const [uname, setUname] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState(false)
+    const handleLogin = () => {
+        if(uname === 'admin@demo' && password === 'admin123')
+        {
+            setIsLogin(true)
+        }
+        else setError(true)
+    }
+  return (
+    <div className="container">
+    <div className="form-1">
+      <h1>AMS3D DEMO</h1>
+      <label for="email">User Name</label>
+      <input type="email" name="User Name" id="email" value ={uname} required onChange={(e)=>setUname(e.target.value)}/>
+      <label for="password">Password</label>
+      <input type="password" name="password" id="password" value={password} required onChange={(e)=>setPassword(e.target.value)}/>
+      <span>Forgot Password</span>
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  </div>
+  )
+}
+
+export default Login
